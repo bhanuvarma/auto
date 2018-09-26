2ece222bfc37bb38f149825982f888d3b35f64f1
bhanuvarma <bhanuhanu99@gmail.com>
2018-09-26 11:47:35 +0530

    kk

--- src/script/Testdemo.java
+++ src/script/Testdemo.java
@@ -1,5 +1,15 @@
 package script;
 
-public class Testdemo {
+import org.junit.Test;
+import org.testng.Reporter;
+
+import generic.BaseTest;
+
+public class Testdemo extends BaseTest{
+	@Test
+	public void testA() {
+		Reporter.log("testA",true);
+	}
+
 
 }
