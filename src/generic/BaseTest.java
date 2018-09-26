f2cb4a8698304553b0686f13547b42db59ac9b08
bhanuvarma <bhanuhanu99@gmail.com>
2018-09-26 11:54:44 +0530

    base

--- src/generic/BaseTest.java
+++ src/generic/BaseTest.java
@@ -4,17 +4,21 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
 	static {
-		System.setProperty("webDriver.chrome.driver","./driver/chromeDriver.exe");
+		System.setProperty("webDriver.chrome.driver","./driver/chromedriver.exe");
 	}
 	WebDriver driver;
+	@BeforeMethod
 	public void openApplication() {
 		driver=new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get("https://facebook.com");
 	}
+	@AfterMethod
    public void closeApplication() {
 	   driver.close();
    }
