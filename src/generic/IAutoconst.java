b46653e225f6fdc5248ba1e62a7bf0ee5223ce5c
bhanuvarma <bhanuhanu99@gmail.com>
2018-09-26 15:36:05 +0530

    bhanu

---
+++ src/generic/IAutoconst.java
@@ -0,0 +1,10 @@
+package generic;
+
+public interface IAutoconst {
+	public static final String CHROME_KEY="webdriver.chrome.driver";
+	public static final String CHROME_VALUE="./driver/chromedriver.exe";
+    String CONFIG_PATH="./config.properties";
+    String XL_PATH="./data/summary.xlsx";
+	
+
+}
