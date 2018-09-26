fa0f519060431b59f150032917dffa1272c85307
bhanuvarma <bhanuhanu99@gmail.com>
2018-09-26 11:36:49 +0530

    bese

--- src/generic/BaseTest.java
+++ src/generic/BaseTest.java
@@ -1,5 +1,18 @@
 package generic;
 
-public class BaseTest {
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+	WebDriver driver;
+	public void openApplication() {
+		driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://facebook.com");
+	}
+   public void closeApplication() {
+	   driver.close();
+   }
 }
